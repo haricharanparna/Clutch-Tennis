db8e3ee24f9f4e4a57c7821dcd2e37bdbfa90f6e
haricharanparna <haricharanparna@gmail.com>
2026-08-26 20:27:33 -0400

    Update booking.py

--- pages/booking.py
+++ pages/booking.py
@@ -11,6 +11,19 @@ st.write("Fill out the form below to request your lesson.")
 
 with st.form("booking_form"):
     name = st.text_input("Full Name")
+    times = [
+        "9:00 AM",
+        "10:00 AM",
+        "11:00 AM",
+        "12:00 PM",
+        "1:00 PM",
+        "2:00 PM",
+        "3:00 PM",
+        "4:00 PM",
+        "5:00 PM",
+        "6:00 PM",
+        "7:00 PM"
+    ]
     preferred_time = st.selectbox(
     "Preferred Time",
     [
